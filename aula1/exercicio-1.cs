c59013896fa581f0b54176c3be02bf13eeb2c995
SOMACAL <crdacostacaio@gmail.com>
2024-03-14 10:53:19 -0300

    adicionando projeto de ambientação e ordenação no repositorio

---
+++ aula1/exercicio-1.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+         int[] vetor = new int[100];
+        Random aleatorio = new Random();
+         for (int )
+        int[] vetor = { 5, 2, 8, 1, 9 };
+
+        // Ordenando o vetor
+        Array.Sort(vetor);
+
+        // Exibindo o vetor ordenado
+        foreach (int num in vetor)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
+   
